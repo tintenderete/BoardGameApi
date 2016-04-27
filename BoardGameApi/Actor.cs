39a26fc6052ff815c78d647db73b2cdfcff6ec06
Julian de Orte <juortegra@gmail.com>
2016-04-27 19:29:50 +0100

    Basic Classes
    Actor, Piece, NoPiece, Cell and Board Classes were added.

---
+++ BoardGameApi/Actor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameApi
+{
+    public abstract class Actor
+    {
+        static int idCount = 0;
+
+        public enum types { Cell, Piece };
+
+        private int type;
+
+        private int id;
+
+        public Actor()
+        {
+            id = idCount;
+            idCount++;
+        }
+
+        public int Get_type()
+        {
+            return type;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
+
+    }
+    
+}
