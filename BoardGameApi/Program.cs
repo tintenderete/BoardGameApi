060e65b6ad32bea2b51375217e3bebc97a6ce090
Julian de Orte <juortegra@gmail.com>
2016-04-27 19:59:50 +0100

    0

--- BoardGameApi/Program.cs
+++ BoardGameApi/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BoardGameApi;
 
 namespace BoardGameApi
 {
     class Program
     {
+
+        
         static void Main(string[] args)
         {
+
+            /// Actor
+
+            //Piece piece = new Piece(Piece.colors.White);
+
+
         }
+        
     }
 }
