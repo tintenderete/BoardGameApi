092d1e3f24b1e85697e2027fa111a8368a1fc564
Julian de Orte <juortegra@gmail.com>
2016-04-28 20:39:01 +0100

    0

--- BoardGameApi/Actor.cs
+++ BoardGameApi/Actor.cs
@@ -9,13 +9,13 @@ namespace BoardGameApi
     public abstract class Actor
     {
         static int idCount = 0;
-<<<<<<< HEAD
+
         public static int GetCount() { return idCount; }
         public static void ResetCount() {idCount = 0; }
 
-=======
+
         static void ResetIdCount() { idCount = 0; }
->>>>>>> refs/remotes/origin/sV-1.0
+
         public enum types { Cell, Piece };
 
         protected int type;
