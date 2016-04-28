51d6835775811378360ba16dd05b3cffeb80bb05
Julian de Orte <juortegra@gmail.com>
2016-04-28 18:10:14 +0100

    0

--- BoardGameApi/Program.cs
+++ BoardGameApi/Program.cs
@@ -13,6 +13,15 @@ namespace BoardGameApi
         
         static void Main(string[] args)
         {
+            // Piece
+            List<Position> PieceMovements = new List<Position>();
+            PieceMovements[0] = new Position(1, 0);
+            PieceMovements[1] = new Position(-1, 0);
+            PieceMovements[2] = new Position(0, 1);
+            PieceMovements[3] = new Position(0, -1);
+
+            Piece whitePiece = new Piece((int)Piece.colors.White, PieceMovements);
+            Piece noPiece = new Piece((int)Piece.colors.White, new List<Position>());
 
             
 
