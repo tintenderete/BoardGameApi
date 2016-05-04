272feaf711f9029dc8045adff966b20b99a9b505
Julian de Orte <juortegra@gmail.com>
2016-05-04 18:01:49 +0100

    Step test
    Static TurnManager is update

--- BoardGameApi/Program.cs
+++ BoardGameApi/Program.cs
@@ -14,6 +14,16 @@ namespace BoardGameApi
         static void Main(string[] args)
         {
 
+
+            Step_Test step_test = new Step_Test();
+
+            if (!step_test.TurnManagerLength_test())
+            {
+                Console.WriteLine("Turn Manager is not update");
+            }
+
+            Console.WriteLine("Test finished");
+            Console.ReadKey();
         }
         
     }
---
+++ BoardGameApi/Step_Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameApi
+{
+    class Step_Test
+    {
+
+        Step step1;
+        Step step2;
+        TurnManager turnManager;
+
+        public Step_Test()
+        {
+            step1 = new PiecesToMove();
+            step2 = new PiecesToMove();
+            turnManager = new TurnManager();
+        }
+
+        public bool TurnManagerLength_test()
+        {
+            turnManager.AddStep(step1);
+
+            if(step1.GetSteps().Count != 1 )
+            {
+                Console.WriteLine("Lenght no 1");
+                return false;
+            }
+
+            turnManager.AddStep(step2);
+
+            if (step1.GetSteps().Count != 2)
+            {
+                Console.WriteLine("Lenght no 2");
+                return false;
+            }
+            
+            return true;
+        }
+        
+            
+    }
+}
