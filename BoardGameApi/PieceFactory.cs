2e6a8b527d8f08e36554e81ecbff271dd6d5dd96
Julian de Orte <juortegra@gmail.com>
2016-05-05 11:11:09 +0100

    mistake repared

--- BoardGameApi/PieceFactory.cs
+++ BoardGameApi/PieceFactory.cs
@@ -17,21 +17,21 @@ namespace BoardGameApi
 
             if (pieceName == (int)names.standard_White)
             {
-                movementSkill[0] = new Position(0, 1);
-                movementSkill[1] = new Position(0, -1);
-                movementSkill[2] = new Position(1, 0);
-                movementSkill[4] = new Position(-1, 0);
-                 
+                movementSkill.Add(new Position(0, 1));
+                movementSkill.Add(new Position(0, -1));
+                movementSkill.Add(new Position(1, 0));
+                movementSkill.Add(new Position(-1, 0));
+
                 newPiece = new Piece((int)Piece.colors.White, movementSkill);
 
                 return newPiece;
             }
             if (pieceName == (int)names.standard_Black)
             {
-                movementSkill[0] = new Position(0, 1);
-                movementSkill[1] = new Position(0, -1);
-                movementSkill[2] = new Position(1, 0);
-                movementSkill[4] = new Position(-1, 0);
+                movementSkill.Add(new Position(0, 1));
+                movementSkill.Add(new Position(0, -1));
+                movementSkill.Add(new Position(1, 0));
+                movementSkill.Add(new Position(-1, 0));
 
                 newPiece = new Piece((int)Piece.colors.Black, movementSkill);
 
