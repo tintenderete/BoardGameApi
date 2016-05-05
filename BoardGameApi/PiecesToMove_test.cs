131b7038d6a6be53a78f8390a21049ef66c7aa8b
Julian de Orte <juortegra@gmail.com>
2016-05-05 19:54:48 +0100

    0

---
+++ BoardGameApi/PiecesToMove_test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameApi
+{
+    class PiecesToMove_test
+    {
+        Board board;
+        PiecesToMove piecesToMove;
+        Player player_W;
+        Player player_B;
+
+        public PiecesToMove_test()
+        {
+            BoardGameFactory factory = new BoardGameFactory();
+
+            board = factory.MakeBoard((int)BoardGameFactory.names.Standard);
+
+            player_W = new Player((int)Player.colors.White);
+            player_B = new Player((int)Player.colors.Black);
+        }
+    }
+}
