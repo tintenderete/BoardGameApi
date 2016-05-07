171d2a6954220215a87d477579e2eb855d94e0e5
Julian de Orte <juortegra@gmail.com>
2016-05-07 15:26:46 +0100

    0

---
+++ BoardGameApi/PiecesToMove_Standard_Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameApi
+{
+    class PiecesToMove_Standard_Test
+    {
+        Board board;
+        Player playerW;
+        Player playerB;
+        PiecesToMove_Standard piecesToMove;
+
+        public PiecesToMove_Standard_Test()
+        {
+            board = new StandardBoardGame();
+            playerW = new Player((int)Player.colors.White);
+            piecesToMove = new PiecesToMove_Standard(board, playerW);
+
+            
+        }
+    }
+}
