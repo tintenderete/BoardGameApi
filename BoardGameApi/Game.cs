7530cf2106ac0df041168bbed8ac18abcac06bf6
Julian de Orte <juortegra@gmail.com>
2016-05-10 18:44:41 +0100

    Game class added

--- BoardGameApi/Game.cs
+++ BoardGameApi/Game.cs
@@ -15,7 +15,45 @@ namespace BoardGameApi
 
         public Game(Board board, TurnManager turnManager )
         {
-           
+            this.board = board;
+            this.turnManager = turnManager;
+            this.currentPlayer = 0;
+            SetPlayers();
+        }
+
+        public void Update()
+        {
+            turnManager.Update();
+        }
+        public Player GetCurrentPlayer()
+        {
+            return players[currentPlayer];
+        }
+
+        public Board GetBoard()
+        {
+            return this.board;
+        }
+
+        public TurnManager GetTurnManager()
+        {
+            return this.turnManager;
+        }
+
+        public void NexPlayer()
+        {
+            currentPlayer++;
+            if (currentPlayer == players.LongLength)
+            {
+                currentPlayer = 0;
+            }
+        }
+
+        public void SetPlayers()
+        {
+            players = new Player[2];
+            players[0] = new Player((int)Player.colors.White);
+            players[1] = new Player((int)Player.colors.Black);
         }
 
     }
--- BoardGameApi/Program.cs
+++ BoardGameApi/Program.cs
@@ -14,7 +14,7 @@ namespace BoardGameApi
         static void Main(string[] args)
         {
 
-            Console.ReadKey();
+           
         }
         
     }
